c75b1cc6852bf3c8b5f80f54c7a0f36f573c098d
Denis Steinhorst <denis@steinhor.st>
2023-12-06 18:37:29 +0100

    add day-03-part1 (push state)

--- src/day-03/parts/part-01.ts
+++ src/day-03/parts/part-01.ts
@@ -4,7 +4,7 @@ export default function (): number {
   let time: number = Date.now();
   let result: number = 0;
   let lineWidth: number = 140;
-  let linesToIterate: number = 5; // max 140
+  let linesToIterate: number = 140; // max 140
 
   // valid symbols too look out for
   const matchingSymbols: { possibilitys: string[], coordinates: number[][] } = {
@@ -80,18 +80,40 @@ export default function (): number {
 
         let foundNumber = false;
   
-        selection.forEach((cell: string[]) => {
+        selection.forEach((cell: string[], index: number) => {
 
-          // check if the first character of the current cell is a number
-          if (!isNaN(parseInt(cell[0]))) {
-            scopeSize.left++;
-            foundNumber = true;
+          if (index === 0) {
+            // assuming we're at the first match of of the current rows[row] (somewhere on the left)
+            // i know that we're moving LEFT if adjecent numbers are found
+            // so i check if the current cell's content is already at the 0th position
+            // of the current row prevent moving out of bounds
+            // just turn the current row and cell around and check if the current cell is at the 0th position
+        
+            if(rows[row].toString().indexOf(cell.toString()) !== -1) {
+
+              // check if the first character of the current cell is a number
+              if (!isNaN(parseInt(cell[0]))) {
+                scopeSize.left++;
+                foundNumber = true;
+              }
+            }
           }
-  
-          // check if the last character of the current cell is a number
-          if (!isNaN(parseInt(cell[cell.length - 1]))) {
-            scopeSize.right++;
-            foundNumber = true;
+
+          if (index === 3) {
+            // assuming we're at the last match of of the current rows[row] (somewhere on the right)
+            // i do the same as above but then for the right side
+
+            const reversedRow = rows[row].toString().split('').reverse().join('');
+            const reversedCell = cell.toString().split('').reverse().join('');
+
+            if(reversedRow[row].toString().indexOf(reversedCell.toString()) !== -1) {
+
+              // check if the last character of the current cell is a number
+              if (!isNaN(parseInt(cell[cell.length - 1]))) {
+                scopeSize.right++;
+                foundNumber = true;
+              }
+            }
           }
         });
 
