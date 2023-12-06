3465e67d2a5f97aa7ce31bf10823d76c7a7d1293
Denis Steinhorst <denis@steinhor.st>
2023-12-06 16:41:34 +0100

    add day-03-part1 (push state)

--- src/day-03/parts/part-01.ts
+++ src/day-03/parts/part-01.ts
@@ -1,131 +1,121 @@
 import data from '../data/part-01.json';
 
-
-
 export default function (): number {
+  let time: number = Date.now();
   let result: number = 0;
   let lineWidth: number = 140;
-  let allCoordinates: number[][] = [];
+  let linesToIterate: number = 5; // max 140
 
   // valid symbols too look out for
-  const matchingSymbols: string[] = data.input.match(/[^\d\s\w\.]/g) || [];
+  const matchingSymbols: { possibilitys: string[], coordinates: number[][] } = {
+    possibilitys: data.input.match(/[^\d\s\w\.]/g) || [],
+    coordinates: []
+  }
 
-  // create 2-dimensional array of the input
+  // 2-dimensional array of input
   let rows: string[][] = (data.input.match(new RegExp(`.{1,${lineWidth}}`, 'g')) || []) as string[][];
 
+  // limit the amount of lines to iterate over
+  rows = rows.slice(0, linesToIterate);
+
+  // coordinates of matching symbols
   rows.map((singleRow: string[], index: number) => {
     
-    // get all coordinates of the matching symbols in the current row, a symbol can occur multiple times
-    // if the excact same coordinates are already in the array, don't add them again
-    matchingSymbols.forEach((symbol: string) => {
+    // loop through all possible symbols
+    matchingSymbols.possibilitys.forEach((symbol: string) => {
       const symbolCoordinates: number[] = [];
-
+ 
+      // loop through all symbols in the current row
       for (let i = 0; i < singleRow.length; i++) {
         if (singleRow[i] === symbol) {
           symbolCoordinates.push(i);
         }
       }
 
+      // add the coordinates of the matching symbols to the array
       symbolCoordinates.forEach((coordinate: number) => {
-        const coordinateAlreadyExists = allCoordinates.find((existingCoordinate: number[]) => existingCoordinate[0] === index && existingCoordinate[1] === coordinate);
+        const coordinateAlreadyExists = matchingSymbols.coordinates.find((existingCoordinate: number[]) => existingCoordinate[0] === index && existingCoordinate[1] === coordinate);
 
         if (!coordinateAlreadyExists) {
-          allCoordinates.push([index, coordinate]);
+          matchingSymbols.coordinates.push([index, coordinate]);
         }
       });
     });
   });
 
-  console.log('🔥 rows:', rows);
+  // loop through the coordinates of matching symbols
+  matchingSymbols.coordinates.forEach((coordinate: number[]) => {
+    const [row, column] = coordinate;
+    // const foundSymbol = rows[row][column];
+
+    // get the current scope of the symbol
+    const currentScope = () => {
+      let currentSelection: string[][] = [];
+
+      // default scopeSize is 1 in each direction (resulting in 3x3 grid around the current symbol)
+      let scopeSize = {
+        left: 1,
+        top: 1,
+        right: 1,
+        bottom: 1
+      }
   
+      // iterate as long as scopeSize keeps increasing
+      while (true) {
 
+        // selection of the current symbol based on the scopeSize
+        const selection: string[][] = rows.slice(row - scopeSize.top, row + scopeSize.bottom + 1).map((singleRow: string[]) => {
+          return singleRow.slice(column - scopeSize.left, column + scopeSize.right + 1);
+        });
 
-  const selectDynamicGrid = (startCoordinate: number[], scopeWidth) => {
-    const [startRow, startCol] = startCoordinate;
-    const { right, left } = scopeWidth;
-    const grid: string[][] = [];
-
-    // loop through the rows
-    for (let i = 0; i < rows.length; i++) {
-      const currentRow = rows[i];
-      const currentRowLength = currentRow.length;
-      const currentRowIndex = i;
-
-      // check if the current row is the row of the starting coordinate
-      if (currentRowIndex === startRow) {
-        // loop through the columns
-        for (let j = 0; j < currentRowLength; j++) {
-          const currentCol = currentRow[j];
-          const currentColIndex = j;
-
-          // check if the current column is the column of the starting coordinate
-          if (currentColIndex === startCol) {
-            // loop through the rows again
-            for (let k = 0; k < rows.length; k++) {
-              const currentRow = rows[k];
-              const currentRowLength = currentRow.length;
-              const currentRowIndex = k;
-              const gridRow: string[] = [];
-
-              // check if the current row is within the scope
-              if (currentRowIndex >= startRow - left && currentRowIndex <= startRow + right) {
-                // loop through the columns again
-                for (let l = 0; l < currentRowLength; l++) {
-                  const currentCol = currentRow[l];
-                  const currentColIndex = l;
-
-                  // check if the current column is within the scope
-                  if (currentColIndex >= startCol - left && currentColIndex <= startCol + right) {
-                    // add the current column to the grid row
-                    gridRow.push(currentCol);
-                  }
-                }
-              }
-
-              // add the grid row to the grid
-              grid.push(gridRow);
-            }
+        // when reached the end of current input, it's possible that the selection is missing a row so we add it
+        while(selection.length < 3) {
+          const charLength = selection[0].length;
+          let contentFiller = '';
+          for (let i = 0; i < charLength; i++) {
+            contentFiller += '.';
           }
+          selection.push((`${contentFiller}` as unknown) as string[]);
+        }
+
+        let foundNumber = false;
+  
+        selection.forEach((cell: string[]) => {
+
+          // check if the first character of the current cell is a number
+          if (!isNaN(parseInt(cell[0]))) {
+            scopeSize.left++;
+            foundNumber = true;
+          }
+  
+          // check if the last character of the current cell is a number
+          if (!isNaN(parseInt(cell[cell.length - 1]))) {
+            scopeSize.right++;
+            foundNumber = true;
+          }
+        });
+
+        currentSelection = selection;
+
+        // Break the loop if no additional numbers were found in the current iteration
+        if (!foundNumber) {
+          return currentSelection;
         }
       }
     }
 
-    return grid.map((row: string[]) => row.join(''));
-  }
+    if (currentScope().length > 0) {
+      const joinedScope = currentScope().join('').replace(/\s/g, '');
+      const extractAdjecentNumbers = joinedScope.match(/\d+/g);
 
-  // loop through every symbol coordinate
-  allCoordinates.forEach((coordinate: number[]) => {
-      const [row, column] = coordinate;
-      const foundSymbol = rows[row][column];
-
-      console.log(`🔥 found: [ ${foundSymbol} ] in row: [ ${row} ] at col: [ ${column} ] `);
-
-      const scopeWidth = {
-        right: 1,
-        left: 1
-      };
-
-      while (rows[row][column - scopeWidth.left] && rows[row][column - scopeWidth.left].match(/\d/)) {
-        scopeWidth.left++;
-      }
-    
-      while (rows[row][column + scopeWidth.right] && rows[row][column + scopeWidth.right].match(/\d/)) {
-        scopeWidth.right++;
-      }
-
-      selectDynamicGrid(coordinate, scopeWidth).forEach((row: string[]) => {
-          if (row.toString().match((/\d/))) {
-            const extractedNumbers = row.toString().split(/[^\d]/).filter((cell: string) => cell !== '');
-      
-            console.log('🔥 extractedNumbers:', extractedNumbers);
-      
-            for (const number of extractedNumbers) {
-              result += parseInt(number);
-            }
-          }
+      extractAdjecentNumbers?.forEach((number: string) => {
+        result += parseInt(number);
       });
+    }
   });
 
+  const timeTaken = new Date(Date.now() - time).toISOString().substr(11, 12);
+  console.log(`🎉 Calculating Result: ${result} took ${timeTaken}`);
 
   return result;
 };
